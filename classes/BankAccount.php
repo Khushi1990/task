979a64485a1c967257a70acf435357ac39da1f6d
Khushi Gosai <khushi.gosai99@gmail.com>
2018-10-10 00:27:49 +0530

    implemented logic to show all tests as "Passed" and in green.

--- classes/BankAccount.php
+++ classes/BankAccount.php
@@ -1,30 +1,39 @@
 <?php
 
-class BankAccount implements IfaceBankAccount
-{
+class BankAccount implements IfaceBankAccount {
 
     private $balance = null;
 
-    public function __construct(Money $openingBalance)
-    {
+    public function __construct(Money $openingBalance) {
         $this->balance = $openingBalance;
     }
 
-    public function balance()
-    {
+    public function balance() {
         return $this->balance;
     }
 
-    public function deposit(Money $amount)
-    {
-        //implement this method
-    }
-	public function withdraw(Money $amount) {
-        
+    public function deposit(Money $amount) {
+        //implement this methodint
+
+        $this->balance = (int) (string) $this->balance + (int) (string) $amount;
+        return $this->balance;
     }
 
-    public function transfer(Money $amount, BankAccount $account)
-    {
-        //implement this method
+    public function withdraw(Money $amount) {
+        if ((int) (string) $this->balance < (int) (string) $amount) {
+            throw new Exception('Withdrawl amount larger than balance');
+        }
+        $this->balance = (int) (string) $this->balance - (int) (string) $amount;
+        return $this->balance;
     }
+
+    public function transfer(Money $amount, BankAccount $account) {
+        if ((int) (string) $amount > (int) (string) $this->balance) {
+            throw new Exception('Withdrawl amount larger than balance');
+        }
+        $this->balance = (int) (string) $this->balance - (int) (string) $amount;
+        $account->balance = (int) (string) $account->balance + (int) (string) $amount;
+        return $this->balance;
+    }
+
 }
