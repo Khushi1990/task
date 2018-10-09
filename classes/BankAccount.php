79ccf4ab21fb16776c7343fd1f2d6c843ad969ac
Khushi Gosai <khushi.gosai99@gmail.com>
2018-10-10 00:25:50 +0530

    added withdraw method

--- classes/BankAccount.php
+++ classes/BankAccount.php
@@ -19,6 +19,9 @@ class BankAccount implements IfaceBankAccount
     {
         //implement this method
     }
+	public function withdraw(Money $amount) {
+        
+    }
 
     public function transfer(Money $amount, BankAccount $account)
     {
